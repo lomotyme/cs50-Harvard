d62b8a4c92cbe09714358166b5647d726b12b6db
lomotyme <lomotyme@users.noreply.github.com>
2020-07-26 07:50:00 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -6,26 +6,26 @@ int main(void)
     int n;
     do
     {
-        n=get_int("Enter an integer between 1 and 8: ");
+        n = get_int("Enter an integer between 1 and 8: ");
     }
-    while(n<1||n>8);
-    for (int i=0;i<n;i++)
+    while (n < 1 || n > 8);
+    for (int i = 0; i < n; i++)
     {
-        for(int j=n;(j-1)>i;j--)
+        for (int j = n; (j-1) > i; j--)
         {
             printf(" ");
 
         }
-        for(int k=-1;k<i;k++)
+        for (int k = -1; k < i; k++)
         {
             printf("#");
         }
         printf("  ");
-        for (int a=-1;a<i;a++)
+        for (int a = -1 ;a < i; a++)
         {
             printf("#");
         }
-        for (int b=n;(b-1)>i;b--)
+        for (int b = n; (b-1) > i; b--)
         {
             printf(" ");
         }
